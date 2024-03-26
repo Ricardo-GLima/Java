f75a26d5d405ab6625bb1dd6a682bae118b91bb0
Ricardo Lima <ricardogithub2@gmail.com>
2024-03-26 20:55:01 -0300

    apostila(git-java):Testar funcao soma

---
+++ CalculadoraTeste.java
@@ -0,0 +1,9 @@
+public class CalculadoraTeste {
+
+    public static void main(String[]args){
+        Calculadora cas = new Calculadora();
+
+        double resultSoma = cas.Calc(5, 6);
+        System.out.println(resultSoma);
+    }
+}
