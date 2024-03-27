4bfe5b98a44024b23ded17e3225d3646e8abfe36
Ricardo Lima <ricardogithub2@gmail.com>
2024-03-26 21:11:29 -0300

    apostila(git-java): Modificando calcPreferido

--- CalculadoraTeste.java
+++ CalculadoraTeste.java
@@ -1,10 +1,16 @@
+import javax.swing.JOptionPane;
+
 public class CalculadoraTeste {
 
     public static void main(String[]args){
-        Calculadora cas = new Calculadora(8);
+        Calculadora cas = new Calculadora(
+            Double.parseDouble(JOptionPane.showInputDialog("Digite seu numero preferido: ")));  
+
         double resultSoma = cas.Calc(5, 6);
         System.out.println(resultSoma);
 
-        System.out.println(cas.CalcPreferido(8));
+        System.out.println(cas.CalcPreferido(
+            Double.parseDouble(JOptionPane.showInputDialog("Digite um numero para somar com o preferido: "))
+        ));
     }
 }
