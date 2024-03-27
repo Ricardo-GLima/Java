ed8c47b2c88fa59de9c5934c5ddbdf35597cf0d9
Ricardo Lima <ricardogithub2@gmail.com>
2024-03-26 21:02:49 -0300

    apostila(git-java): funcao calcular preferido

--- Calculadora.java
+++ Calculadora.java
@@ -1,6 +1,18 @@
 public class Calculadora {
 
+    private double meuNumeroPreferido;
+
+    public Calculadora(double meuNumeroPreferido){
+        this.meuNumeroPreferido = meuNumeroPreferido;
+    }
+ 
     public double Calc(double v1, double v2){
         return v1 + v2;
+    }
+    public double Sub(double v1, double v2){
+        return v1 - v2;
+    }
+    public double CalcPreferido(double v1){
+        return v1 + meuNumeroPreferido;
     }
 }
--- CalculadoraTeste.java
+++ CalculadoraTeste.java
@@ -1,9 +1,10 @@
 public class CalculadoraTeste {
 
     public static void main(String[]args){
-        Calculadora cas = new Calculadora();
-
+        Calculadora cas = new Calculadora(8);
         double resultSoma = cas.Calc(5, 6);
         System.out.println(resultSoma);
+
+        System.out.println(cas.CalcPreferido(8));
     }
 }
