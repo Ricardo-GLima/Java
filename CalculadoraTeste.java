2377ba8e36d7b7769f8586461c869511660c6460
Ricardo Lima <ricardogithub2@gmail.com>
2024-03-26 21:21:54 -0300

    apostila(git-java):Criando menu interativo

--- CalculadoraTeste.java
+++ CalculadoraTeste.java
@@ -5,12 +5,25 @@ public class CalculadoraTeste {
     public static void main(String[]args){
         Calculadora cas = new Calculadora(
             Double.parseDouble(JOptionPane.showInputDialog("Digite seu numero preferido: ")));  
-
-        double resultSoma = cas.Calc(5, 6);
-        System.out.println(resultSoma);
-
-        System.out.println(cas.CalcPreferido(
-            Double.parseDouble(JOptionPane.showInputDialog("Digite um numero para somar com o preferido: "))
-        ));
+            String ac = JOptionPane.showInputDialog("Digite a operação: ");
+            
+            switch (ac) {
+                case "soma":
+                        double resultSoma = cas.Calc(5, 6);
+                        System.out.println(resultSoma);
+                    break;
+                case "sub":
+                        double resultSub = cas.Sub(15, 8);
+                        System.out.println(resultSub);
+                    break;
+                case "somap":
+                         System.out.println(cas.CalcPreferido(
+                         Double.parseDouble(JOptionPane.showInputDialog("Digite um numero para somar com o preferido: "))
+                        ));
+                    break;
+                default:
+                        System.out.println("Erro");
+                    break;
+            }         
     }
 }
